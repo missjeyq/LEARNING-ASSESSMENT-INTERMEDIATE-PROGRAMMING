45ae638afddae8592a7983997e9d8d26a0a5bea3
Em-Jhaye Pelicano <emjhayepelicano1297@gmail.com>
2026-02-14 10:58:19 +0800

    Add temperature checker functionality

---
+++ temperature_checker.py
@@ -0,0 +1,9 @@
+print(“Temperature Checker”)
+temp = int(input("Enter your current temperature: "))
+
+if temp > 30:
+    print("Hot Day! Stay Hyrdated.")
+else:
+    print("Pleasant Weather!")
+if temp < 10:
+    print("Oww its cold!")
