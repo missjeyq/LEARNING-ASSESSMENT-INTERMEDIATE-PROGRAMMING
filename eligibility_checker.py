5b73154916d539c2851ee3ced68c9cd00cd2e338
Em-Jhaye Pelicano <emjhayepelicano1297@gmail.com>
2026-02-14 11:54:02 +0800

    Update eligibility_checker.py

--- eligibility_checker.py
+++ eligibility_checker.py
@@ -2,9 +2,9 @@ print("Eligibility Checker")
 
 user_age = int(input("Enter user age: "))
 valid_id = input("Do you have a valid ID? yes/no: ")
-has_id = valid_id == "yes"
+id_check = valid_id == "yes"
 
-if user_age >= 18 and has_id:
+if user_age >= 18 and id_check:
     print("Eligible")
 else:
     print("Not Eligble")
