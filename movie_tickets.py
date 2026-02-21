8a5fcc853231d5a8ca93710b5d9e3d3faaa3858f
Em-Jhaye Pelicano <emjhayepelicano1297@gmail.com>
2026-02-21 12:22:40 +0800

    Add files via upload

--- movie_tickets.py
+++ movie_tickets.py
@@ -3,14 +3,14 @@ print("\n=== Movie Ticket System ===")
 day = input("Enter days (weekends or weekdays): ")
 type = input("Enter customer type (regular or student or senior): ")
 time = input("Enter movie time: ")
-tickets = int(input("Enter number of tickets: "))
+tickets = input("Enter number of tickets: ")
 
 if day == "weekdays":
-    price = float(200) * tickets
+    price = 200 * tickets
     print(f"Base price: 200 * {tickets} = {price}")
 
 else:
-    price = float(300) * tickets
+    price = 300 * tickets
     print(f"Base price: 300 * {tickets} = {price}")
 
     if type == "student":
@@ -25,4 +25,17 @@ else:
         price = price - discounted_price
         print(f"Senior discount (30%): {discounted_price}")
 
-    
+if time < 12:
+    discount = 0.10
+    discounted_price = price * discount
+    price = price - discounted_price
+    print(f"Matinee discount (10%): {discounted_price}")
+
+if tickets > 5:
+    discount = 0.05
+    discounted_price = price * discount
+    price = price - discounted_price
+    print(f"Group discount (5%): {discounted_price}")   
+
+print(f"\nTOTAL: {price:.2f}")
+print("Thank you for you purchased!")
