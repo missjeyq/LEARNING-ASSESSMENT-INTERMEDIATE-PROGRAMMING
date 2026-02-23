00095dfd7e37ca1dc5db4dc5de1f2aac3a566c94
Em-Jhaye Pelicano <emjhayepelicano1297@gmail.com>
2026-02-23 20:58:01 +0800

    grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -6,34 +6,34 @@ if score < 0 or score > 100:
 
 elif score <= 100 and score >= 94:
     grade = "A+"
-    message = "Very Good"
+    message = "Excellent"
 elif score <= 93 and score >= 90:
     grade = "A-"
-    message = "Nice"
+    message = "Amazing"
 elif score <= 89 and score >= 87:
     grade = "B+"
-    message = "Good"
+    message = "Awesome"
 elif score <= 86 and score >= 83:
     grade = "B"
-    message = "Ayos"
+    message = "Very Good"
 elif score <= 82 and score >= 80:
     grade = "B-"
-    message = "Pede ka ha"
+    message = "Nice"
 elif score <= 79 and score >= 77:
     grade = "C+"
-    message = "Okay na din"
+    message = "Good"
 elif score <= 76 and score >= 73:
     grade = "C"
-    message = "Solid"
+    message = "Passed"
 elif score <= 72 and score >= 70:
     grade = "C-"
-    message = "Ngii"
+    message = "Passed"
 elif score <= 69 and score >= 67:
     grade = "D+"
     message = "Passed"
 elif score <= 66 and score >= 60:
     grade = "D"
-    message = "Ayy"
+    message = "Passed"
 elif score <= 59:
     grade = "F"
     message = "Failed"
